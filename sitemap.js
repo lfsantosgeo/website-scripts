4005550c144bb7968c0da31ec0fc17c6df48da61
Luiz Fernando dos Santos <39103053+lfsantosgeo@users.noreply.github.com>
2023-12-05 21:34:02 -0300

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -10,7 +10,7 @@
 
 /*
 var toc_config = {
-	url: 'https://www.twistblogg.com',
+	url: 'https://geoterran.blogspot.com',
 	containerId: 'tb-sitemap',
 	showNew: 15,
 	newText: ' <strong style="font-weight:normal;font-style:normal;color:#fff;font-size:11px;background:#5c5a78;padding:1px 6px 3px 6px;line-height:normal;float:right;border-radius:3px;">New!</strong>',
